e776c5b4981375666ec7aed2e556c5b652e09cdc
Hasan Khan <slashdot10@gmail.com>
2014-04-20 21:33:29 -0700

    * Read version from package.json * Push mssql into args if no args are provided to show correct usage in help * Changed version to 0.0.8

--- bin/mssql.js
+++ bin/mssql.js
@@ -3,7 +3,7 @@
 var argv = process.argv.splice(0);
 // if no arguments were specified then show help
 if (argv.length == 2) {
-    argv = ['', '', '-h']
+    argv = ['', 'mssql', '-h']
 }
 var cli = new SqlCli();
 cli.run(argv);
