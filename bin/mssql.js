f3e511625d87012012a86fb824ac22199d6f2227
Hasan Khan <slashdot10@gmail.com>
2014-05-18 18:44:44 -0700

    added support for reading connection information from environment variables or config file

--- bin/mssql.js
+++ bin/mssql.js
@@ -1,9 +1,5 @@
 ﻿var SqlCli = require('../lib/cli');
 
 var argv = process.argv.splice(0);
-// if no arguments were specified then show help
-if (argv.length == 2) {
-    argv = ['', 'mssql', '-h']
-}
 var cli = new SqlCli();
 cli.run(argv);
