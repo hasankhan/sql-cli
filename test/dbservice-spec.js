129b08303d682f2ab83fb9e59f86486d0da4f238
Hasan Khan <slashdot10@gmail.com>
2014-05-25 10:09:06 +0500

    * added jasmine test for DbService * added jshint in test script * fixed jshint errors * moved exit.js out to external folder to avoid jshint warnings

---
+++ test/dbservice-spec.js
@@ -0,0 +1,35 @@
+var assert = require('assert'),
+    proxyquire =  require('proxyquire')
+    DbService = require('../lib/dbservice');
+
+describe('DbService', function() {
+    var mssql, service;
+    
+    beforeEach(function() {
+        mssql = {};
+        var DbService = proxyquire('../lib/dbservice', { 'mssql': mssql });
+        service = new DbService();
+    });
+    
+    it('connects to sql on connect', function(done) {
+        mssql.connect = function (config, callback) { callback(); };
+        
+        service.connect()
+                .then(done)
+                .fail(done);
+    });
+    
+    it('query formats and concats the args', function(done) {
+        var Request = mssql.Request = function(){};
+        Request.prototype.query = function (query, callback) { callback ( null, [{colA: 'theValue'}]); };
+        spyOn(Request.prototype, 'query').andCallThrough();
+        
+        service.query('%s are %d tests', ['there', 123])
+               .then(function(results){
+                    expect(results.length).toBe(1);
+                    expect(results[0].colA).toBe('theValue');
+                    expect(Request.prototype.query).toHaveBeenCalledWith('there are 123 tests', jasmine.any(Function));
+                    done();
+               }).fail(done);
+    });
+});
