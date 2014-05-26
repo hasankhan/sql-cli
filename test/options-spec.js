d48eeb0aa6e75f8bd1b0a6b8fe66c4850d92ac75
Hasan Khan <slashdot10@gmail.com>
2014-05-26 06:24:39 +0500

    added more tests for options.js

--- test/options-spec.js
+++ test/options-spec.js
@@ -1,22 +1,25 @@
 var proxyquire =  require('proxyquire');
 
 describe('Options', function() {
-    var fs, path, options;
-    
+    var fs, path, options, config;
+
     beforeEach(function() {
         fs = {};
         path = {};
+        config = { '@noCallThru': true };
         var Options = proxyquire('../lib/options', { 
-            fs: fs, 
-            path: path 
+            'fs': fs, 
+            'path': path,
+            'mssql-conf.json': config,
+            'test-conf.json': config
         });
         options = new Options();
     });
-    
-    describe('init', function() {
+
+    describe('init', function() {    
         it('parses and copies args', function() {
-            fs.existsSync = function() { return true; };
-            
+            fs.existsSync = jasmine.createSpy().andReturn(true);
+
             options.init([
                 'node.exe', 'test.js',
                 '-s', 'localhost',
@@ -31,24 +34,24 @@ describe('Options', function() {
                 '-f', 'xml',
                 '-c', 'config.json'
             ], {});       
-            
+
             expect(options.args).toEqual({
-                'server': 'localhost',
-                'user': 'sa',
-                'pass': 'password',
-                'port': '1234',
-                'timeout': '5500',
-                'database': 'test',
-                'query': '.tables',            
-                'tdsVersion': '7_2',
-                'encrypt': true,
-                'format': 'xml',
-                'config': 'config.json'
+                server: 'localhost',
+                user: 'sa',
+                pass: 'password',
+                port: '1234',
+                timeout: '5500',
+                database: 'test',
+                query: '.tables',
+                tdsVersion: '7_2',
+                encrypt: true,
+                format: 'xml',
+                config: 'config.json'
             });
         });    
         
         it('throws if config does not exist', function() {
-            fs.existsSync = function() { return false; };
+            fs.existsSync = jasmine.createSpy().andReturn(false);
             
             var args = [
                 'node.exe', 'test.js',
@@ -57,6 +60,62 @@ describe('Options', function() {
             
             var err = new Error("config file 'config.json' does not exist.");
             expect(options.init.bind(options, args, {})).toThrow(err);               
+        });
+    });
+
+    describe('getConnectionInfo', function() {
+        it('defaults the config file name to mssql-conf.json', function () {
+            path.resolve = jasmine.createSpy().andReturn('mssql-conf.json');
+            fs.existsSync = jasmine.createSpy().andReturn(true);
+            
+            options.getConnectionInfo();
+
+            expect(fs.existsSync).toHaveBeenCalledWith('mssql-conf.json');
+        });
+        
+        it('defaults the user and server name', function () {
+            path.resolve = jasmine.createSpy().andReturn('mssql-conf.json');
+            fs.existsSync = jasmine.createSpy().andReturn(true);
+            
+            var info = options.getConnectionInfo();
+
+            expect(info.server).toEqual('localhost');
+            expect(info.user).toEqual('sa');
+        });
+        
+        it('creates the config object', function () {
+            path.resolve = jasmine.createSpy().andReturn('test-conf.json');
+            fs.existsSync = jasmine.createSpy().andReturn(true);
+
+            options.init([
+                'node.exe', 'test.js',
+                '-s', 'example.com',
+                '-u', 'theuser',
+                '-p', 'thepass',
+                '-o', '5400',
+                '-t', '1000',
+                '-d', 'catalog',
+                '-q', '.tables',
+                '-v', '7_3',
+                '-e', 
+                '-f', 'json',
+                '-c', 'test-conf.json'
+            ], {});                               
+
+            var info = options.getConnectionInfo();
+
+            expect(info).toEqual({
+                user: 'theuser',
+                password: 'thepass',
+                server: 'example.com',
+                database: 'catalog',
+                port: '5400',
+                timeout: '1000',
+                options: {
+                    tdsVersion: '7_3',
+                    encrypt: true
+                }
+            });
         });
     });
 });
