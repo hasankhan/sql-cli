87a0b651298ff89adc5f0107b3fb0a19b1f1962e
Hasan Khan <slashdot10@gmail.com>
2014-05-27 06:33:51 -0700

    added tests for prompt.js

---
+++ test/prompt-spec.js
@@ -0,0 +1,58 @@
+var proxyquire =  require('proxyquire').noPreserveCache();
+
+describe('Options', function() {
+    var prompt, rl;
+
+    function setup() {
+        beforeEach(function() {
+            rl = {
+                setPrompt: jasmine.createSpy(),
+                on: jasmine.createSpy()
+            };
+
+            var readline = {
+                '@noCallThru': true
+            };
+
+            readline.createInterface = jasmine.createSpy().andReturn(rl)
+
+            var Prompt = proxyquire('../lib/prompt', {
+                'readline': readline
+            });
+
+            prompt = new Prompt();
+        });
+    }
+
+    describe('ctor', function() {
+        setup();
+
+        it('sets prompt to mssql', function() {
+            expect(rl.setPrompt).toHaveBeenCalledWith('mssql> ');
+            expect(rl.on).toHaveBeenCalledWith('line', jasmine.any(Function));
+        });
+    });
+
+    describe('next', function() {
+        setup();
+
+        it('fires close event if exit is set', function (done) {
+            var code = -123;
+
+            prompt.exit = true;
+            prompt.on('end', function(exitCode) {
+                expect(exitCode).toEqual(code);
+                done();
+            });
+
+            prompt.next(code);
+        });
+
+        it('prompts on next', function (done) {
+            rl.prompt = function() {
+                done();
+            };
+            prompt.next();
+        });
+    });
+});
