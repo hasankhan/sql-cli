90ce9cee49ba27b8e19201166fafd33b46881731
Muhammad Hasan Khan <slashdot10@gmail.com>
2016-10-23 09:44:07 -0700

    use .batch instead of .query to maintain session across batches

--- test/mssqldb-spec.js
+++ test/mssqldb-spec.js
@@ -40,7 +40,7 @@
             it('executes the query', function (done) {
                 var Request = mssql.Request = function () { };
                 Request.prototype.on = jasmine.createSpy();
-                Request.prototype.query = jasmine.createSpy();
+                Request.prototype.batch = jasmine.createSpy();
 
                 service.query('there are 123 tests')
                     .then(function (results) {
@@ -50,7 +50,7 @@
                         expect(Request.prototype.on).toHaveBeenCalledWith('recordset', jasmine.any(Function));
                         expect(Request.prototype.on).toHaveBeenCalledWith('row', jasmine.any(Function));
                         expect(Request.prototype.on).toHaveBeenCalledWith('error', jasmine.any(Function));
-                        expect(Request.prototype.query).toHaveBeenCalledWith('there are 123 tests');
+                        expect(Request.prototype.batch).toHaveBeenCalledWith('there are 123 tests');
                         done();
                     }).catch(done);
 
