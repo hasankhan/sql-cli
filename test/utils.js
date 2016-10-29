14df08b825ab327ebf26666abbaf9655654fd4a1
Muhammad Hasan Khan <slashdot10@gmail.com>
2016-10-29 08:18:46 -0700

    formatted utils.js

--- test/utils.js
+++ test/utils.js
@@ -1,22 +1,22 @@
 (function (utils) {
     // removes the module from require cache
-    utils.unloadModule = function(name) {
+    utils.unloadModule = function (name) {
         // convert name into regex if it is not
         if (!(name instanceof RegExp)) {
             name = new RegExp(name.toString());
         }
 
         Object.keys(require.cache)
-              .forEach(function(key) {
+            .forEach(function (key) {
                 // if module has name in it then remove it
                 if (name.test(key)) {
                     delete require.cache[key];
                 }
-              });
+            });
     };
 
     // normalizes and compares two strings for equality
-    utils.stringEqual = function(first, second) {
+    utils.stringEqual = function (first, second) {
         var pattern = /[\s\r\n\t]/g;
         var normalizedFirst = first.replace(pattern, '');
         var normalizedSecond = second.replace(pattern, '');
